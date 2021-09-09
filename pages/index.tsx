a3a89a3fddcfa1e876f6df86c641412f0103284d
Adi Narayana Vemuru <vemuruadi@gmail.com>
2021-09-09 12:56:06 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Feedback app built for Fresh Qube</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          
+            Next.js powered website!
+          
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{' '}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            pages/index.tsx
+          </code>
+        </p>
+
+        
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vemuruadi.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Next.JS website starter boilerplate  by Adi Vemuru{' '}
+          
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
